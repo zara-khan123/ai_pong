474d753b511f773d6ceaf13048d8b18eaf6918e4
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-07-08 16:20:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -50,10 +50,21 @@ function gotPoses(results)
   }
 }
 
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
+}
+
+
+
 
 function draw(){
 
- background(0); 
+  if(game_status == "start")
+  {
+    background(0); 
+    image(video, 0, 0, 700, 600);
 
  fill("black");
  stroke("black");
