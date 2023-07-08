1496005ce42037bff74f77f3e6c44698923949b3
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-07-08 16:16:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -38,6 +38,18 @@ function modelLoaded()
 }
 
 
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
 
 function draw(){
 
